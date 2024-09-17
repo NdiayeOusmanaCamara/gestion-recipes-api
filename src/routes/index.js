ef0b2ebc544bdb574290b2a19718c4c38ba99aae
Mangassouba <hamallahmanga@gmail.com>
2024-09-17 01:08:19 +0000

    ajout le module validator

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import RecipeController from '../controlleur/recipes.js';
+import  {addRequestValidator,deleteRequestValidator,updateValidator,getByIdValidator}  from '../validators/recipeValidator.js'; 
 
 const router = express.Router();
 
 router.get('/recipes', RecipeController.getAllRecipes);
-router.get('/recipes/:id', RecipeController.getRecipeById);
-router.post('/recipes', RecipeController.createRecipe);
-router.put('/recipes/:id', RecipeController.updateRecipe);
-router.delete('/recipes/:id', RecipeController.deleteRecipe);
+router.get('/recipes/:id',getByIdValidator, RecipeController.getRecipeById);
+router.post('/recipes',addRequestValidator, RecipeController.createRecipe);
+router.put('/recipes/:id',updateValidator, RecipeController.updateRecipe);
+router.delete('/recipes/:id',deleteRequestValidator, RecipeController.deleteRecipe);
 
 export default router;
---
+++ src/validators/recipeValidator.js
@@ -0,0 +1,112 @@
+import { check, param, validationResult } from "express-validator";
+import { StatusCodes } from "http-status-codes";
+import  Recipe  from "../model/Recipe.js";
+
+const addRequestValidator = [
+  check("titre")
+    .not()
+    .isEmpty()
+    .withMessage("Titre ne peut pas être vide!")
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage("Minimum 6 caractères requis!")
+    .bail()
+    .custom(async(value, { req }) => {
+        const result = await Recipe.checkRecipe(value)
+        if(result !== 0){
+            throw new Error('Cette recette existe déjà!')
+        }
+        return true;
+      }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
+    next();
+  },
+];
+
+const deleteRequestValidator = [
+    param("id")
+      .not()
+      .isEmpty()
+      .withMessage("Id est obligatoire!")
+      .bail()
+      .custom(async(value, { req }) => {
+          const result = await Recipe.getRecipeById(value)
+          if(result === 0){
+              throw new Error("Cette recette n'existe pas!")
+          }
+          return true;
+        }),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
+      next();
+    },
+  ];
+
+  const getByIdValidator = [
+    param("id")
+      .not()
+      .isEmpty()
+      .withMessage("L'ID est obligatoire.")
+      .bail()
+      .custom(async (value) => {
+        const result = await Recipe.getRecipeById(value);
+        if (!result) {
+          throw new Error("Cette recette n'existe pas.");
+        }
+        return true;
+      }),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(StatusCodes.UNPROCESSABLE_ENTITY)
+          .json({ errors: errors.array() });
+      }
+      next();
+    },
+  ];
+
+
+  const updateValidator = [
+    param("id")
+      .not()
+      .isEmpty()
+      .withMessage("L'ID est obligatoire.")
+      .bail()
+      .custom(async (value) => {
+        const result = await Recipe.getRecipeById(value);
+        if (!result) {
+          throw new Error("Cette recette n'existe pas.");
+        }
+        return true;
+      }),
+    check("titre")
+      .optional()
+      .isLength({ min: 6 })
+      .withMessage("Le titre doit contenir au moins 6 caractères."),
+    check("type")
+      .optional()
+      .not()
+      .isEmpty()
+      .withMessage("Le type ne peut pas être vide."),
+    check("ingredients")
+      .optional()
+      .isArray({ min: 1 })
+      .withMessage("Les ingrédients doivent être une liste."),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(StatusCodes.UNPROCESSABLE_ENTITY)
+          .json({ errors: errors.array() });
+      }
+      next();
+    },
+  ];
+
+export { addRequestValidator, deleteRequestValidator,updateValidator, getByIdValidator };
